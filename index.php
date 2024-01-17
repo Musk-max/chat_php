1f71fb343794b37d4a0e7c0e246a1dee79909109
Musk-max <76408168+Musk-max@users.noreply.github.com>
2024-01-17 13:01:19 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>connexion</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="" class="form_connection_inscription">
+        <h1>Connexion</h1>
+        <p class="message_error">
+            Mot de passe incorrect
+        </p>
+        <label for="">adresse mail</label>
+        <input type="email" name="email" >
+        <label for="">Mot de passe</label>
+        <input type="password" name="mdp1" id="">
+        <input type="submit" value="connexion">
+        <p class="link">Avez vous un compte ? <a href="#">creer un compte </a></p>
+    </form>
+    
+</body>
+</html>
